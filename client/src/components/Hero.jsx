9a39ffad4984e72a645723ba08225da0195c96ea
sai patnam <saipatnam99@gmail.com>
2024-11-05 20:26:31 +0530

    changes

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
 import heroImage from '../assets/profile.jpeg'
-import {TypeAnimation} from 'react-type-animation'
+import { TypeAnimation } from 'react-type-animation'
+
 const Hero = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8 bg-black'>
-     <div className='col-span-1 my-auto mx-auto w-[300px] h-auto lg:w-[400px]'>
-        
-            <img src={heroImage} alt="hero Image" />
-        
-     </div>
-     <div className='col-span-2 px-5'>
-     <h1 className='text-white text-4xl sm:text-5xl lg:text-8xl font-extrabold'>
-        <span className='primary-color'>I'm a </span> <br/>
-        <TypeAnimation sequence={[
-            "Frontend Dev", 1000,
-            "FullStack Dev",1000,
-        ]}
-        wrapper='span'
-        speed={50}
-        repeat={Infinity}/>
-
-     </h1>
-     <p className='text-white sm:text-lg my-6 lg:text-xl'>
-        My Name is Sai Patnam and I am fresher with having skills on Fullstack Devlopment.
+      <div className='col-span-1 my-auto mx-auto w-[300px] h-auto lg:w-[400px]'>
+        <img src={heroImage} alt="hero Image" />
+      </div>
+      <div className='col-span-2 px-5'>
+        <h1 className='text-white text-4xl sm:text-5xl lg:text-8xl font-extrabold'>
+          <span className='primary-color'>I'm a </span> <br />
+          <TypeAnimation
+            sequence={[
+              "Frontend Dev", 1000,
+              "FullStack Dev", 1000,
+            ]}
+            wrapper='span'
+            speed={50}
+            repeat={Infinity}
+          />
+        </h1>
+        <p className='text-white sm:text-lg my-6 lg:text-xl'>
+          My Name is Sai Patnam and I am fresher with having skills in Fullstack Development.
         </p>
         <div className='my-8'>
-            <a href="/" className='px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-500 to-pink-500 text-white'>
-            Download CV</a>
-            <a href="#contact" className='px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-500 to-pink-500 text-white hover:border-none'>
-            Contact</a>
+          <a
+            href="/"
+            className='px-4 py-2 sm:px-6 sm:py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-500 to-pink-500 text-white text-center'>
+            Download CV
+          </a>
+          <a
+            href="#contact"
+            className='px-4 py-2 sm:px-6 sm:py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-500 to-pink-500 text-white text-center hover:border-none'>
+            Contact
+          </a>
         </div>
-     </div>
+      </div>
     </div>
   )
 }
--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -5,35 +5,45 @@ import javascript from '../assets/javascript.png'
 import tailwind from '../assets/tailwind.png'
 import react from '../assets/react.png'
 import mongodb from '../assets/mongodb.jpg'
+
 const Skills = () => {
   return (
-    <div className='border border-gray-600 bg-black text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-6 place-items-center md:flex md:justify-between md:items-center sm:flex sm:flex-cols-2'>
+    <div className='border border-gray-600 bg-black text-gray-400 md:h-[150px] max-w-[1200px] mx-auto sm:border-none
+                    grid grid-cols-3 sm:grid-cols-3 md:grid-cols-6 place-items-center md:flex md:justify-between md:items-center'>
      
-     <h2 className='text-gray-700 text-2xl md:text-4xl font-bold m-4'>My <br/> Tech <br/> Stack</h2>
+     <h2 className='text-gray-700 text-2xl md:text-4xl font-bold m-4 col-span-3 md:col-span-1 text-center'>
+       My <br/> Tech <br/> Stack
+     </h2>
+
      <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
         <img src={html} alt="html" height={100} width={100} />
         <p className='mt-2'>HTML</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+     </div>
+     
+     <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
         <img src={css} alt="css" height={100} width={100} />
         <p className='mt-2'>CSS</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+     </div>
+     
+     <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
         <img src={javascript} alt="javascript" height={100} width={100} />
-        <p className='mt-2'>javascript</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+        <p className='mt-2'>JavaScript</p>
+     </div>
+     
+     <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
         <img src={react} alt="react" height={100} width={100} />
         <p className='mt-2'>React</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+     </div>
+     
+     <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
         <img src={tailwind} alt="tailwind" height={100} width={100} />
         <p className='mt-2'>Tailwind</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+     </div>
+     
+     <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
         <img src={mongodb} alt="mongodb" height={100} width={100} />
         <p className='mt-2'>MongoDB</p>
-        </div>
+     </div>
     </div>
   )
 }
